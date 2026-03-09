85dbc411dd77b805a019782de4796c433ee9ff60
Karthik <karthik6262@gmail.com>
2026-03-10 01:39:00 +0530

    fix: use explicit relative include path for App.h in shard.cpp

--- cpp_gateway/src/shard.cpp
+++ cpp_gateway/src/shard.cpp
@@ -2,7 +2,7 @@
 #include "tick_normalizer.h"
 #include "zmq_publisher.h"
 #include "redis_writer.h"
-#include <App.h> // uWebSockets
+#include "../../deps/uWebSockets/src/App.h" // uWebSockets
 #include <iostream>
 #include <chrono>
 
